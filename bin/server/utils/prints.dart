68f1fd1ddb2ae03bd3ee6d1962604f9041b61a7c
Arj-Lab <arj.lemited@gmail.com>
2024-10-23 19:14:38 +0300

    first commit

--- bin/server/utils/prints.dart
+++
@@ -1,7 +0,0 @@
-void printWarning(String text) {
-  print('\x1B[33m$text\x1B[0m');
-}
-
-void printError(String text) {
-  print('\x1B[31m$text\x1B[0m');
-}
--- bin/utils/validation.dart
+++ bin/utils/validation.dart
@@ -1,13 +1,12 @@
 import 'dart:convert';
 import 'package:crypto/crypto.dart';
 import 'package:hex/hex.dart';
-import '../global_constants.dart';
 
 
 bool validation(String payload, String hash) {
   var dataCheckString = payload;
   var hmacSecretKey = Hmac(sha256, utf8.encode('WebAppData'));
-  var secretKey = hmacSecretKey.convert(utf8.encode(GlobalConstants.botToken));
+  var secretKey = hmacSecretKey.convert(utf8.encode('7138294827:AAHil30m8k0oLvVZWjammBLJWSBUmGNRfw8'));
   var hashSecretKey = Hmac(sha256, secretKey.bytes);
   var checkHash = hashSecretKey.convert(utf8.encode(dataCheckString));
   if (HEX.encode(checkHash.bytes) != hash) {
